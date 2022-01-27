ab3dc9c51e68526aee4869574b250bc99589f43b
Clark Fitzgerald <clarkfitzg@gmail.com>
2022-01-27 14:34:43 -0800

    .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 22spring-stat196k
+
+These are public notes and data sets for class, Stat 196K in Spring 2022 at CSUS taught by Clark Fitzgerald.
---
+++ tiny.sh
@@ -0,0 +1,15 @@
+# Thu Jan 27 22:17:55 UTC 2022
+# Clark Fitzgerald
+
+# Make some tiny versions of the GDELT events table for examples.
+
+TINY="tiny_events.tsv"
+
+# Start with a header, because original data doesn't have a header, unfortunately.
+echo -e "GLOBALEVENTID\tYEAR\tActor1Name\tEventCode\tNumArticles\tSOURCEURL" > ${TINY}
+
+# Append 20 rows of data for the corresponding columns.
+head -n 20 first100events.txt | cut -f 1,4,7,27,34,61 >> ${TINY}
+
+# Translate tab characters to commas to get an actual CSV file.
+cat ${TINY} | tr "\t" "," > tiny_events.csv
