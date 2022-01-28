91fa572cbda0671b21af93a65eb134ce3334862e
Clark Fitzgerald <clarkfitzg@gmail.com>
2022-01-28 00:26:44 +0000

    .

--- tiny.sh
+++ tiny.sh
@@ -9,7 +9,7 @@ TINY="tiny_events.tsv"
 echo -e "GLOBALEVENTID\tYEAR\tActor1Name\tEventCode\tNumArticles\tSOURCEURL" > ${TINY}
 
 # Append 20 rows of data for the corresponding columns.
-head -n 20 first100events.txt | cut -f 1,4,7,27,34,61 >> ${TINY}
+head -n 20 /examples/first100events.txt | cut -f 1,4,7,27,34,61 >> ${TINY}
 
 # Translate tab characters to commas to get an actual CSV file.
 cat ${TINY} | tr "\t" "," > tiny_events.csv
